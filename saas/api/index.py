b484fde7b453706af026d473e4e7db4a831ec172
Ayush Shekhar <ayushshekhar@Ayushs-MacBook-Pro-2.local>
2025-09-28 11:46:13 +0200

    added clerk JWT verification

--- saas/api/index.py
+++ saas/api/index.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import StreamingResponse
 from openai import OpenAI
 import os
+from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials 
+
+clerk_config = ClerkConfig(jwks_url=os.getenv("CLERK_JWKS_URL"))
+clerk_guard = ClerkHTTPBearer(clerk_config)
 
 api_key = os.environ.get("GOOGLE_API_KEY")
 
@@ -14,12 +18,14 @@ client = OpenAI(
 app = FastAPI()
 
 @app.get("/api")
-def idea():
+def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
     try:
+        user_id = creds.decoded["sub"]
         messages = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
         stream = client.chat.completions.create(
                         model="gemini-2.5-flash",
-                        messages=messages 
+                        messages=messages ,
+                        stream=True
                     )
         def event_stream():
             for chunk in stream:
@@ -28,7 +34,8 @@ def idea():
                     lines = text.split("\n")
                     for line in lines:
                         yield f"data: {line}\n"
-                    yield "\n"
+                        yield "data:  \n"
+                    yield f"data: {lines[-1]}\n\n"
 
         return StreamingResponse(event_stream(), media_type="text/event-stream")
     except Exception as e:
